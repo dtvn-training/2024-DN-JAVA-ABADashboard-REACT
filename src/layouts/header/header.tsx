bce7a202eb3970794701d3d5ed511a1bc015a992
nguyenhieu31 <hieufaptv777@gmail.com>
2025-01-10 23:49:51 +0700

    delete file

--- src/layouts/header/header.tsx
+++
@@ -1,9 +0,0 @@
-const Header= ()=>{
-    return (
-        <header>
-            <h1>Welcome to the ABA Dashboard</h1>
-            <p>This is a dashboard for the ABA organization</p>
-        </header>
-    )
-}
-export default Header;
